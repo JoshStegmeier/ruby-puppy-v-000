ffdc210d8d2d4e880ac3e6cd0786ab3d3695cb2c
Josh Stegmeier <josh@steg.me>
2018-06-03 18:53:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+  #The @@all class variables stores an array of all dogs.
+  @@all = []
+
+  #Allowing read/write privledges of 'name'
+  attr_accessor :name
+
+
+  #Runs during instantiation.
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+
+
 end
